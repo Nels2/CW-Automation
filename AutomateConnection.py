ef5cddf3fe017482966a0bf1979a92f65f35e6a6
Nelson Orellana <55712980+Nels2@users.noreply.github.com>
2021-02-09 16:00:13 -0600

    CW.py replaces 'main.py'

--- AutomateConnection.py
+++ AutomateConnection.py
@@ -4,6 +4,7 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 import time
 from time import sleep
 import re
@@ -13,8 +14,8 @@ import pyfiglet
 from pyfiglet import Figlet
 import termcolor
 from termcolor import colored, cprint
-import main
-from main import computerz
+import CW
+from CW import computerz
 
 
 # -- -- -- -- -- -- -- -- -- #
@@ -25,16 +26,20 @@ from main import computerz
 
 # --
 # This script is intended to login into Automate and grab the verification code from an email to login.
-# Should be used before 'main.py'
+# Should be used before 'CW.py'
+# Working to be able to use to complete scripts 
 # --
 print_blue = lambda x: cprint(x, 'blue')
 print_yellow = lambda x: cprint(x, 'yellow')
 print_red = lambda x: cprint(x, 'red')
 print_green = lambda x: cprint(x, 'green')
 
+now = datetime.datetime.now()
+
 url = "https://seamlessdata.hostedrmm.com/automate/login"
 driver = webdriver.Firefox()
 driver.get(url)
+pre = "[" + now.strftime('%Y-%m-%d %H:%M:%S') + "]: "
 # time to make this easy to use for anyone..
 print("------------------------------------------------------------------")
 # change the below fields to match your login info.
@@ -48,28 +53,33 @@ print_yellow("#### --------- Begin Automate Connection --------- ####")
 print("")
 custom_fig = Figlet(font='hollywood')
 print_red(custom_fig.renderText('B'))
-
-
-print_blue("[BrinxBot]: starting out.. login in to Automate is first task... commencing...")
+print_blue(pre + "[BrinxBot]: starting out.. login in to Automate is first task... commencing...")
 NextDay_Date = datetime.datetime.today() + datetime.timedelta(days=1)
 time.sleep(3)
 enter_user = driver.find_element_by_id('loginUsername')
-enter_user.send_keys(usrname + Keys.RETURN)
-time.sleep(2)
-pw = driver.find_element_by_id('loginPassword')
-pw.send_keys(passwd + Keys.RETURN)
+time.sleep(0.5)
+enter_user.send_keys(usrname) # for some reason usrname + Keys.RETURN does not owkr on this script but works fine with CW.py... will just click 'Next' instead of sending return,
+time.sleep(1.5) # wait because automate loads for no reason when youre done typing
+click_next = driver.find_element_by_css_selector("#root > div > div > div.login-login > div > div:nth-child(3) > div.CwButton-wrap > div").click() 
 time.sleep(1.5)
-print("[BrinxBot]: We just need the token now to login! Opening new tab... and switching to it!")
+pw = driver.find_element_by_id('loginPassword')
+time.sleep(0.5) 
+pw.send_keys(passwd + Keys.RETURN)
+print_green("#### -- Automate Control Center Login Sumbitted.. Awaiting Token.. -- ####")
+#time.sleep(0.3)---------------------------------------------------------------------------------------------------------------------------------------||
+#click_login = driver.find_element_by_css_selector("#root > div > div > div.login-login > div > div:nth-child(5) > div:nth-child(1) > div").click() -----uncomment if Keys.RETURN is not working in Line 65.
+time.sleep(1.5)
+print_blue(pre + "[BrinxBot]: We just need the token now to login! Opening new tab... and switching to it!")
 # now to login into my email and get code and go back to verify. Must be done withing 3-8 minutes or code is no longer valid.
 # --- second tab --- #
 # the 2 lines after this comment are meant to collect the current window handle info. --#
 # Opening Tab 2 up [Office 365 Email Inbox]
 first_tab_handle = driver.current_window_handle
 print_yellow("first_tab_handle : "+str(first_tab_handle))
-print_blue("[BrinxBot]: Logging into Office... ")
+print_blue(pre + "[BrinxBot]: Logging into Office... ")
 driver.execute_script("window.open('about:blank', 'tab2');")
 driver.switch_to.window("tab2")
-print_blue("[BrinxBot]: Switched to second tab. Focus is here currently.")
+print_blue(pre + "[BrinxBot]: Switched to second tab. Focus is here currently.")
 driver.get('https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1611956433&rver=7.0.6737.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fnlp%3d1%26RpsCsrfState%3de00d1cdc-7140-348d-ccae-406a5464dec6&id=292841&aadredir=1&CBCXT=out&lw=1&fl=dob%2cflname%2cwld')
 time.sleep(2)
 
@@ -78,17 +88,17 @@ epwd = ''
 
 userN = driver.find_element_by_name('loginfmt')
 userN.send_keys(email + Keys.RETURN)
-time.sleep(3)
+time.sleep(2.5)
 passwd = driver.find_element_by_name('passwd')
 passwd.send_keys(epwd + Keys.RETURN)
 passwd.send_keys(Keys.RETURN)
-
+print_green("#### -- Office Account Has Been Signed in. -- ####")
 # select 'No' to stay signed in.
 time.sleep(1.5)
 no = driver.find_element_by_css_selector("#idBtn_Back").click()
-print_blue("[BrinxBot]: No. I dont want to stay signed in... continuing")
+print_blue(pre + "[BrinxBot]: No. I dont want to stay signed in... continuing")
 # time to open the email and grab the code...
-print_blue("[BrinxBot]: I'm in! Going to look for the email and save the code..")
+print_blue(pre + "[BrinxBot]: I'm in! Going to look for the email and save the code..")
 time.sleep(7)
 WebDriverWait(driver, 200).until(EC.presence_of_element_located((By.CLASS_NAME, 'BVgxayg_IGpXi5g7S77GK')))
 search_email = driver.find_element_by_css_selector('._1Qs0_GHrFMawJzYAmLNL2x')
@@ -99,27 +109,28 @@ time.sleep(2)
 save_it = driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div[1]/div/div/div/div[3]/div[2]/div/div[3]/div/div/div/div/div[2]/div/div[1]/div/div/div/div[3]").text
 print_yellow(save_it)
 da_code = re.sub(r"\D", "", save_it)
-print_blue("[BrinxBot]: I have split the original message to just the code!: " + da_code)
+print_blue(pre + "[BrinxBot]: I have split the original message to just the code!: " + da_code)
 # now to switch back to tab 1.. [Automate Login Screen]
-print_blue("[BrinxBot]: ...switching back to Automate Login Screen and inserting code to login")
+print_blue(pre + "[BrinxBot]: ...switching back to Automate Login Screen and inserting code to login")
 time.sleep(3)
 driver.switch_to.window(first_tab_handle) # automate login
 time.sleep(2)
 click_on_token = driver.find_element_by_id('loginToken')
 click_on_token.send_keys(da_code + Keys.RETURN)
-print_yellow("#### Login was successful ####")
+print_yellow("#### -- Automate Control Center Connecton Established... -- ####")
 # check variable to see if it is the same. 
 computer = pickle.load( open( "save.p", "rb"))
-print_yellow("#### Pickle has loaded in the following saved variable from main: " + computer)
-print_green("[AC][BrinxBot]: I'm in! Looking for computer: " + computer + "!")
+print_yellow("#### -- Pickle has loaded in the following saved variable from main: " + computer + " -- #####")
+print_green(pre + "[AC][BrinxBot]: I'm in! Looking for computer: " + computer + "!")
 # now time to search for computer and double click on it
 print_yellow("#### .....searching in automate.... ####")
 time.sleep(6)
 search_for_comp = driver.find_element_by_xpath("/html/body/div/div/div/div/div[4]/div[2]/div[2]/div[3]/div[2]/div/span[1]/div/div[2]/input")
-print_blue("[BrinxBot]: Computer has been found clicking on it to continue the task...")
+print_blue(pre + "[BrinxBot]: Computer has been found clicking on it to continue the task...")
 search_for_comp.send_keys(computer + Keys.RETURN)
-element = driver.find_element_by_css_selector("#root > div > div > div > div.browse-container > div.company-container > div.company-content > div:nth-child(3) > div.CwDataGrid-rowsContainer > div > div > span:nth-child(1) > div").click()
-# save this tab so i can return to it
+time.sleep(3)
+select_computer = driver.find_element_by_css_selector("#root > div > div > div > div.browse-container > div.company-container > div.company-content > div:nth-child(3) > div.CwDataGrid-rowsContainer > div > div").click()
+# save this tab so i can return to it in case a new window is launched.
 second_tab_handle = driver.current_window_handle
 print_yellow("second_tab_handle : "+str(second_tab_handle)) # right before computer screen 
 
@@ -127,20 +138,20 @@ print_yellow("second_tab_handle : "+str(second_tab_handle)) # right before compu
 script_start = driver.find_element_by_css_selector("#root > div > div > div > div.browse-container > div.company-container > div.company-content > div.CwToolbar-cwToolbar.CwGridToolbar-container > div.CwGridToolbar-leftContainer > div.ComputersGridWithToolbar-scriptsButton > div > div > div > div > div").click()
 time.sleep(0.5)
 script_search = driver.find_element_by_css_selector("#root > div > div > div > div.browse-container > div.company-container > div.company-content > div.CwToolbar-cwToolbar.CwGridToolbar-container > div.CwGridToolbar-leftContainer > div.ComputersGridWithToolbar-scriptsButton > div > div:nth-child(2) > div > input")
-print_green("[BrinxBot]: ..script has been found.")
+print_green(pre + "[BrinxBot]: ..script has been found.")
 script_search.send_keys("reboot script")
 script_run = driver.find_element_by_css_selector("#root > div > div > div > div.browse-container > div.company-container > div.company-content > div.CwToolbar-cwToolbar.CwGridToolbar-container > div.CwGridToolbar-leftContainer > div.ComputersGridWithToolbar-scriptsButton > div > div:nth-child(2) > div > div.CwTreeDropdown-treeContainer > div > div > div.CwTreeViewNode-subTree > div > div > label").click()
 
-print_blue("[BrinxBot]: Inside " + computer + " menu now, launching script...")
+print_blue(pre + "[BrinxBot]: Inside " + computer + " menu now, launching script...")
 
 # -- wait for dialog box to appear.. -- #
-print_blue("[BrinxBot]: waiting for dialog box..")
+print_blue(pre + "[BrinxBot]: waiting for dialog box..")
 WebDriverWait(driver, 200).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div/div/div[4]/div[2]/div[2]/div[2]/div[1]/div[2]/div[1]/div[2]/div')))
 # -- check the do later box -- #
-print_blue("[BrinxBot]: checking the 'do late' option so the script runs at a different time.")
+print_blue(pre + "[BrinxBot]: checking the 'do late' option so the script runs at a different time.")
 do_later = driver.find_element_by_css_selector("#browse_computers_grid_toolbar_button_scripts_now_later_dialogscrollable_body_id > div.ScriptSchedulerDialog-radioButtonContainer > div.CwRadioButtonGroup-container > div:nth-child(2) > div > div > div > svg > circle.CwRadioButton-largeInnerCircle").click()
 # change the date for script to be ran at, usually at the moment or the next day at 12:00:00 AM
-print_blue("[BrinxBot]: Changing the date to tomorrow.")
+print_blue(pre + "[BrinxBot]: Changing the date to tomorrow.")
 date = driver.find_element_by_css_selector("#browse_computers_grid_toolbar_button_scripts_now_later_dialogscrollable_body_id > div.ScriptSchedulerDialog-timeDateFields > div:nth-child(1) > div.CwDatePicker-datePickerRoot > div > input")
 date.send_keys(Keys.CONTROL + 'a' + Keys.DELETE)
 
@@ -150,20 +161,20 @@ NextDay_Date_Formatted = NextDay_Date.strftime ('%m' + '-' + '%d' + '-' + '%Y') 
 print_yellow('#### tomorrows date is ' + str(NextDay_Date_Formatted))
 time.sleep(1)
 date.send_keys(str(NextDay_Date_Formatted))
-print_green("[BrinxBot]: Date has been changed.")
+print_green(pre + "[BrinxBot]: Date has been changed.")
 # --------------------------------------
 # change time script is ran to 12:00:00 AM
-print_blue("[BrinxBot]: Changing the time to 12a")
+print_blue(pre + "[BrinxBot]: Changing the time to 12a")
 times = driver.find_elements_by_css_selector("#browse_computers_grid_toolbar_button_scripts_now_later_dialogscrollable_body_id > div.ScriptSchedulerDialog-timeDateFields > div:nth-child(2) > input")
 times.send_keys(Keys.CONTROL + 'a' + Keys.DELETE)
 time.sleep(1)
 times.send_keys("12a" + Keys.RETURN)
-print_green("[BrinxBot]: Time has been changed")
+print_green(pre + "[BrinxBot]: Time has been changed")
 # -------------------------------
 # click SCHEDULE
-print_blue("[BrinxBot]: Wrapping this up & selecting OK...")
+print_blue(pre + "[BrinxBot]: Wrapping this up & selecting OK...")
 click_ok = driver.find_element_by_css_selector("#root > div > div > div > div.browse-container > div.company-container > div.company-content > div.CwToolbar-cwToolbar.CwGridToolbar-container > div.CwGridToolbar-leftContainer > div.ComputersGridWithToolbar-scriptsButton > div.Dialogs-dialogContainer > div.CwScrollableDialog-scrollableDialogContainer > div > div > div.CwDialog-buttons > div > div.ScriptSchedulerDialog-cancelNextContainer > div:nth-child(2) > div").click()
-print_green("[BrinxBot]: I have completed the task assigned... shutting off after letting server know...")
+print_green(pre + "[BrinxBot]: I have completed the task assigned... shutting off after letting server know...")
 # establish external connection to let server know job completed right
 def Server_ReReConnect():
     try:
@@ -175,10 +186,11 @@ def Server_ReReConnect():
 
         time.sleep(1)
         Connection = driverTwo.find_element_by_css_selector("#message")
-        Connection.send_keys("Computer ticket has been completed successfully in CWA!" + Keys.RETURN)
+        Connection.send_keys("/name CWABrinxBot" + Keys.RETURN)
+        Connection.send_keys("Computer ticket has been completed successfully in ConnectWise Automate Control Center!" + Keys.RETURN)
     except RuntimeError:
-        print_red("Server Connection Failed. Continuing with shutdown")
+        print_red(pre + "Server Connection Failed. Continuing with shutdown")
     driverTwo.quit()
 Server_ReReConnect()
-print_red("$- Connection has been lost to BrinxBot.")
+print_red(pre + "Connection has been lost to BrinxBot.")
 print("---------------------------------------------------------------")
